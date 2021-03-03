06f4b8ec148e68032083cbb4c7552caee97e005c
marianos <marianos8125@gmail.com>
2021-03-03 16:36:34 -0300

    add exception handling

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -43,7 +43,7 @@ public class CustomerServiceImpl implements CustomerService{
                     customerDTO.setCustomerUrl(getCustomerUrl(id));
                     return customerDTO;
                 })
-                .orElseThrow(RuntimeException::new); //todo implement better exception handling
+                .orElseThrow(ResourceNotFoundException::new);
     }
 
     @Override
@@ -85,7 +85,7 @@ public class CustomerServiceImpl implements CustomerService{
 
             return  returnDto;
 
-        }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
+        }).orElseThrow(ResourceNotFoundException::new); //todo implement better exception handling;
     }
 
     private String getCustomerUrl(Long id) {
