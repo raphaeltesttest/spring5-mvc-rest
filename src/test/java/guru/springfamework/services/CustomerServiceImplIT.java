aa521a753c9316dc4fd23298d4cb533f4f7b0fd7
marianos <marianos8125@gmail.com>
2021-03-06 17:51:01 -0300

    add mapper and service test

--- src/test/java/guru/springfamework/services/CustomerServiceImplIT.java
+++ src/test/java/guru/springfamework/services/CustomerServiceImplIT.java
@@ -6,6 +6,7 @@ import guru.springfamework.bootstrap.Bootstrap;
 import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +31,9 @@ public class CustomerServiceImplIT {
 
     @Autowired
     CategoryRepository categoryRepository;
+
+    @Autowired
+    VendorRepository vendorRepository;
 
     CustomerService customerService;
 
