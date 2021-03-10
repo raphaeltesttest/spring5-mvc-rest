e9d97b767138e5f56c0ea4774b191acdd0cb640d
marianos <marianos8125@gmail.com>
2021-03-09 22:41:43 -0300

    customize swagger with meta data

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -2,15 +2,21 @@ package guru.springfamework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig { //extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
@@ -19,6 +25,28 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("xxxxx", "htpps://xxxxx.com/about", "xxxxx@springframwork.guru");
+
+                return new ApiInfo(
+                        "Spring Framework Guru",
+                        "Spring Framework 5: Beginner to Guru",
+                        "1.0",
+                        "Terms of Service: blah",
+                        contact,
+                        "Apache License Version2.0",
+                        "https://www.apache.org/licenses/LICENSE-2.0",
+                        new ArrayList<>());
+    }
+
+////    use this method to manually configure swagger-ui (non-springboot project)
+////    springboot automatically configure this
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        super.addResourceHandlers(registry);
+//    }
 }
